758112b0c6ca0ce82063f1ea5a20be53f03d61b6
Виталий <ipbvv@mail.ru>
2025-07-30 14:48:18 +0300

    Modifies the opening and closing of a database connection

--- page_analyzer/data_base.py
+++ page_analyzer/data_base.py
@@ -4,10 +4,11 @@ from psycopg2.extras import DictCursor
 
 class UrlsRepo:
     def __init__(self, url_db):
-        self.conn = psycopg2.connect(url_db)
+        self.url_db = url_db
+#        self.conn = psycopg2.connect(url_db)
 
     def get_all_urls(self):
-        with self.conn.cursor(cursor_factory=DictCursor) as cur:
+        with psycopg2.connect(self.url_db).cursor(cursor_factory=DictCursor) as cur:
             query = """
             SELECT
                 urls.id,
@@ -27,35 +28,35 @@ class UrlsRepo:
             """
             cur.execute(query)
             urls = cur.fetchall()
-#            self.conn.commit()
         return urls
 
     def add_url(self, url):
-        with self.conn.cursor(cursor_factory=DictCursor) as cur:
+        conn = psycopg2.connect(self.url_db)
+        with conn.cursor(cursor_factory=DictCursor) as cur:
             cur.execute(
                 'INSERT INTO urls (name) VALUES (%s) RETURNING id;',
                 (url,)
             )
             url_id = cur.fetchone()
-            self.conn.commit()
-            return url_id['id']
+            conn.commit()
+        conn.close()
+        return url_id['id']
 
     def find_same_url(self, url):
-        with self.conn.cursor(cursor_factory=DictCursor) as cur:
+        with psycopg2.connect(self.url_db).cursor(cursor_factory=DictCursor) as cur:
             cur.execute('SELECT id FROM urls WHERE name = (%s);', (url,))
             url_id = cur.fetchone()
-
         return url_id['id'] if url_id else None
 
     def get_url(self, url_id):
-        with self.conn.cursor(cursor_factory=DictCursor) as cur:
+        with psycopg2.connect(self.url_db).cursor(cursor_factory=DictCursor) as cur:
             cur.execute('SELECT * FROM urls WHERE id = %s;', (url_id,))
             result = cur.fetchone()
-#            self.conn.commit()
         return result
 
     def add_check(self, check):
-        with self.conn.cursor(cursor_factory=DictCursor) as cur:
+        conn = psycopg2.connect(self.url_db)
+        with conn.cursor(cursor_factory=DictCursor) as cur:
             query = """
             INSERT INTO url_checks  (
                 url_id,
@@ -75,13 +76,13 @@ class UrlsRepo:
             """
             cur.execute(query, check)
             check_id = cur.fetchone()
-            self.conn.commit()
+            conn.commit()
+        conn.close()
         return check_id['id']
 
     def get_url_checks(self, url_id):
-        with self.conn.cursor(cursor_factory=DictCursor) as cur:
+        with psycopg2.connect(self.url_db).cursor(cursor_factory=DictCursor) as cur:
             query = 'SELECT * FROM url_checks WHERE url_id = %s;'
             cur.execute(query, (url_id,))
             url_checks = cur.fetchall()
-#            self.conn.commit()
         return url_checks
